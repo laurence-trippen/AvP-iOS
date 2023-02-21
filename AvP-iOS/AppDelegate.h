45a9b7db1664318b9165b2a2a6f03c2a75201295
Laurence Trippen <laurence.trippen@smartelephantapps.com>
2023-02-21 21:26:10 +0400

    Initial Commit

---
+++ AvP-iOS/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  AvP-iOS
+//
+//  Created by Laurence Trippen on 21.02.23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AvP-iOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AvP-iOS
+//
+//  Created by Laurence Trippen on 21.02.23.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  NSString * appDelegateClassName;
+  @autoreleasepool {
+      // Setup code that might create autoreleased objects goes here.
+      appDelegateClassName = NSStringFromClass([AppDelegate class]);
+  }
+  return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
