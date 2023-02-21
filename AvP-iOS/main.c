3a79eb259e69d390c90604360a9080b7711f5c8a
Laurence Trippen <laurence.trippen@smartelephantapps.com>
2023-02-21 22:50:35 +0400

    SDL & OpenGL ES application scaffold

--- AvP-iOS/main.c
+++ AvP-iOS/main.c
@@ -5,6 +5,11 @@
 //  Created by Laurence Trippen on 21.02.23.
 //
 
+#include <stdbool.h>
+
+#include <OpenGLES/ES2/gl.h>
+#include <OpenGLES/ES2/glext.h>
+
 #include "SDL.h"
 
 int HandleAppEvents(void *userdata, SDL_Event *event)
@@ -49,9 +54,61 @@ int HandleAppEvents(void *userdata, SDL_Event *event)
   }
 }
 
+void iosRenderCallback(void* window)
+{
+  // Render
+  glClearColor(255.0f, 255.0f, 0.0f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+  SDL_GL_SwapWindow((SDL_Window*)window);
+}
+
 int main(int argc, char *argv[])
 {
+  // Register handler for iOS Lifecycle hooks
   SDL_SetEventFilter(HandleAppEvents, NULL);
+  
+  // Init SDL Subsystems
+  if (SDL_Init(SDL_INIT_VIDEO) < 0)
+  {
+    SDL_Log("Couldn't initialize SDL Video!\n");
+  }
+  
+  SDL_DisplayMode displayMode;
+  SDL_GetDesktopDisplayMode(0, &displayMode);
+  
+  SDL_Window* window = SDL_CreateWindow(NULL, 0, 0, displayMode.w, displayMode.h, SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN | SDL_WINDOW_RESIZABLE);
+  if (!window)
+  {
+    SDL_Log("Couldn't initialize SDL Window!\n");
+  }
+  
+  SDL_GLContext glContext = SDL_GL_CreateContext(window);
+  
+  SDL_iPhoneSetAnimationCallback(window, 1, iosRenderCallback, window);
+  
+  // Game Loop
+  SDL_Event event;
+  bool done = false;
+  
+  while (!done)
+  {
+    SDL_PumpEvents();
+    
+    while (SDL_PollEvent(&event))
+    {
+      switch (event.type)
+      {
+        case SDL_QUIT:
+          done = true;
+          break;
+      }
+    }
+  }
+  
+  // Cleanup
+  SDL_GL_DeleteContext(glContext);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
   
   return 0;
 }
